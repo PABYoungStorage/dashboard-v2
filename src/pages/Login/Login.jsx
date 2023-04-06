3a11e1db455482613fa651183d973551701e63a6
nocturnalplay <nocturnalplay0@gmail.com>
2023-04-07 02:11:14 +0530

    0 auth added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,67 @@
 import { useState } from "react";
 import "./login.css";
-import { useNavigate } from "react-router-dom";
-
+import { json, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [data,setData] = useState({
-        username:"",
-        password:"",
-    })
-    const navigate = useNavigate();
-    const submit = ()=>{
-        if(data.username === "akshaya" && data.password === "akshaya123"){
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+  const [otp, setotp] = useState({
+    status: false,
+    value: "",
+  });
+  const navigate = useNavigate();
+  const otpverify = async () => {
+    if (otp.status && otp.value) {
+      let login = await fetch("http://localhost:3000/api/verify-otp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          otp: otp.value,
+        }),
+      }).then((res) => res.json());
+      Object.keys(login).map((a) => {
+        if (a == "status") {
+          if (login[a] == true) {
+            alert("Successfully verified");
             navigate("/dashboard");
+          } else {
+            alert(login.message);
+          }
         }
+      });
     }
+  };
+  const submit = async () => {
+    // if (data.username === "akshaya" && data.password === "akshaya123") {
+    //   navigate("/dashboard");
+    // }else{
+    //   alert("Incorrect username or password")
+    // }
+    if (data.username && data.password) {
+      let login = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: data.username,
+          password: data.password,
+        }),
+      }).then((res) => res.json());
+      Object.keys(login).map((a) => {
+        if (a == "status") {
+          if (login[a] == true) {
+            alert("Successfully login");
+            setotp((a) => ({ ...a, status: true }));
+          } else {
+            alert(login.message);
+          }
+        }
+      });
+    }
+  };
   return (
-    <div className="login-class">
+    <>
       <head>
         <title>Attirant Login</title>
 
@@ -29,25 +75,69 @@ const Login = () => {
           rel="stylesheet"
         />
       </head>
+      <div className="login-class">
+        <div className="background">
+          <div className="shape"></div>
+          <div className="shape"></div>
+        </div>
+        <form>
+          <img src="violet white-01.png" id="login-img" alt="" srcSet="" />
 
-      <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
+          {otp.status || (
+            <>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Email or Phone"
+                id="username"
+                onChange={(e) =>
+                  setData((a) => ({ ...a, [e.target.name]: e.target.value }))
+                }
+                value={data.username}
+              />
+
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                id="password"
+                onChange={(e) =>
+                  setData((a) => ({ ...a, [e.target.name]: e.target.value }))
+                }
+                value={data.password}
+              />
+
+              <button type="button" onClick={submit}>
+                Log In
+              </button>
+            </>
+          )}
+          {otp.status && (
+            <>
+              <label htmlFor="otp">OTP Verify</label>
+              <input
+                type="number"
+                name="otp"
+                placeholder="otp"
+                id="otp"
+                onChange={(e) =>
+                  setotp((a) => ({ ...a, value: e.target.value }))
+                }
+                value={otp.value}
+              />
+
+              <button type="button" onClick={otpverify}>
+                Verify
+              </button>
+            </>
+          )}
+          {/* <a href="/dashboard">Log In</a> */}
+        </form>
       </div>
-      <form>
-        <img src="violet white-01.png" id="login-img" alt="" srcset="" />
-
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Email or Phone" id="username" onChange={(e)=>setData(a=>({...a,[e.target.name]:e.target.value}))} value={data.username} />
-
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Password" id="password" onChange={(e)=>setData(a=>({...a,[e.target.name]:e.target.value}))} value={data.password} />
-
-        <button onClick={submit}>Log In</button>
-        {/* <a href="/dashboard">Log In</a> */}
-      </form>
-    </div>
+    </>
   );
 };
 
-export default Login
+export default Login;
